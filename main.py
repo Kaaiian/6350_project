c82b6ca7533664ea3752f7c84419621eb7afc5e0
Kaai Kauwe <kaaikauwe@gmail.com>
2018-03-30 11:20:09 -0600

    fixed main.py df.column name effected for loop

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from Formula import *
 from FeatureMatrix import *
 
+formula_list = [['NaCl'], ['Ag'], ['LiAsI']]
+formula_column = pd.DataFrame(formula_list)
+
 if __name__ == "__main__":
 
     element_data = pd.read_csv(r'simple_element_properties.csv', index_col=0)
 
     feature_matrix = FeatureMatrix()
+    column_name = 0
+    formula_column = formula_column[column_name]
     for formula in formula_column:
         new_formula = Formula(formula, element_data)
         feature_matrix.addFormula(new_formula.get_feature_vector())
     feature_matrix.createDataFrame()
     print(feature_matrix.get_df_features())
+    
+    formula_column[0]
